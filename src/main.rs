5a2b081502a033c871a133821f37c0eeedf9c484
Nishant-Pall <nishant@loopinhq.com>
2022-05-03 00:02:44 +0530

    Use clap to fetch args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused)]
+
+use clap::Parser;
+
+#[derive(Parser)]
+/// Search for a pattern in a file and display the lines that contain it.
+struct Cli {
+    // The pattern to look for
+    pattern: String,
+
+    // The path to file to be read
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // The parse method is meant to be used in your main function.
+    // When it fails, it will print out an error or help message and
+    // immediately exit the program. Don’t use it in other places!
+
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
