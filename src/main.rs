6816af5510bafd25cb6d0218fade848373ee66b6
Nishant-Pall <nishant@loopinhq.com>
2022-05-03 00:34:50 +0530

    Use map_err for error reading

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,9 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+#[derive(Debug)]
+struct CustomError(String);
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // The parse method is meant to be used in your main function.
     // When it fails, it will print out an error or help message and
@@ -26,8 +29,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     // You can append this operator to a value of type Result,
     // and Rust will internally expand this to something very similar to the match we just wrote.
-    let result = std::fs::read_to_string(&args.path)?;
 
+    let path = "test.txt";
+
+    let result = std::fs::read_to_string(&args.path)
+        .map_err(|err| CustomError(format!("Error reading: `{}`: {}", path, err)));
     // let content = match result {
     //     Ok(content) => content,
     //     Err(error) => return Err(error.into()),
