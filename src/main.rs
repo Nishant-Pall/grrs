c57ccec92abb76d5cabcc3615fd1de309145d783
Nishant-Pall <palln6935@gmail.com>
2022-05-15 20:02:41 +0530

    Write tests

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,18 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
+
+use grrs::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing arguments: {}", err);
+        eprintln!("Problem passing arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+    if let Err(e) = grrs::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
